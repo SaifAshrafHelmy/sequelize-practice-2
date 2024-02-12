5126fe6edd0776149b3c5582b77a2fa7183ef53c
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2024-02-12 16:55:04 +0200

    Using basic model method + Added pino logger

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const logger = require('./logger.js');
 require('dotenv').config();
 
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
@@ -8,10 +9,69 @@ const sequelize = new Sequelize(connection_uri);
 async function main() {
   try {
     await sequelize.authenticate();
-    console.log('database connection initialized successfully');
+    logger.info('database connection initialized successfully');
+    initializeDatabaseModels();
+    await sequelize.sync({ force: true });
+    logger.info('All models were synchronized successfully.');
+    console.log('--------------------------------------------------');
+    await sequelize.drop();
+    logger.info('All tables dropped!');
+
+    await sequelize.close();
+    logger.info(
+      'All operations done and DB connection was closed successfully.'
+    );
   } catch (error) {
-    console.log('Unable to connect to the database: ', error);
+    logger.fatal('Unable to connect to the database: ', error);
   }
 }
 
+const initializeDatabaseModels = async () => {
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  });
+  logger.info('USER MODEL DROPPED.');
+};
+
 main();
+
+// class User extends Model {}
+
+// User.init(
+//   {
+//     firstName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     lastName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     age: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+//   },
+//   {
+//     sequelize,
+//     modelName: 'User',
+//   }
+// );
+
+// const user = new User({
+//   firstName: 'Saif',
+//   lastName: 'Ashraf',
+// });
+
+// console.log(user);
